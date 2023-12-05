07c35e8deb4265b72ab8e9595c7c9d15cccb5edb
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-12-05 15:12:27 +0100

    Aggiunto contatore Js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-// Test funzionamento
+console.log('JS-FIZZBUZZ')
 
-console.log('Template html css js')
+const list = document.getElementById('list');
+console.log ('lista ' + list +' '+ typeof list);
+
+for (let x = 1 ; x <= 100 ; x++) {
+    const myLi = document.createElement('li');
+    myLi.append(x);
+
+    const resto3 = x % 3;
+    const resto5 = x % 5;
+
+    const div3= resto3 == 0
+    const div5= resto5 == 0
+
+    if (div3 && div5) {
+        console.log(x + ' Fizz AND Buzz')
+    }
+
+    else if (div3) {
+        console.log(x + ' Fizz')
+    }
+
+    else if (div5) {
+        console.log(x + ' Buzz')
+    }
+
+    else {
+        console.log(x)
+    }
+}
 
